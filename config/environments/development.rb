e8369a55ffc51401ca359028030dc724ca436193
fdantas78 <development@dantas.me>
2017-01-11 12:52:58 +0000

    Setup email confirmation functionality for new users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'learning-ruby-fdantas.c9users.io', :protocol => 'https'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +40,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
 end
